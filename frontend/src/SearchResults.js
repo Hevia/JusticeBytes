1d73608acd973015fedbb235f9ab3c1b2c4c6f28
Hevia <anthony@hevia.dev>
2020-09-26 18:20:32 -0400

    Got dynamically sized search results page

--- frontend/src/SearchResults.js
+++ frontend/src/SearchResults.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import * as React from "react";
+import './App.css';
+import {
+  Card,
+  StyledBody,
+  StyledAction
+} from "baseui/card";
+import { Button } from "baseui/button";
 
 function SearchResults(props) {
   return (
-  <div>{console.log(props.stateContext.resultsFound)}</div>
+  <div>
+    <ul>
+        {
+            props.stateContext.searchResults.get.map(searchResult => (
+                <Card className="search-result-card">
+                    <StyledAction>
+                    <Button
+                    overrides={{
+                        BaseButton: { style: { width: "50%" } }
+                    }}
+                    href={searchResult.url}
+                    >
+                    {searchResult.title}
+                    </Button>
+                </StyledAction>
+                </Card>
+            ))
+        }
+    </ul>
+</div>
   );
 }
 
