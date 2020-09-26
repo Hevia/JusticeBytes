c8ccfbeacae7ef674068bda89e0c72d1fd17254d
Adam Fernandes <fernadam8@gmail.com>
2020-09-26 00:09:29 -0400

    put in testing and able to output contents of the webpage

--- backend/src/etc/web_scraper.py
+++ backend/src/etc/web_scraper.py
@@ -1,6 +1,7 @@
 # Adam Fernandes
 # ShellHacks 2020
 
+from bs4 import BeautifulSoup
 import requests
 
 # URL bases which will be the beginning of all URLS
@@ -12,20 +13,20 @@ KEYWORDS_FILENAME: str = "keywords.txt"
 fileObject = open(KEYWORDS_FILENAME, "r")
 
 # Returns true if a link is a valid webpage, false otherwise
-def isValidLink(link: str):
-   website = requests.get(link)
-
+def isValidLink(link: requests.models.Response):
    try:
-      website.raise_for_status()
+      link.raise_for_status()
    except:
       print("Error in %s." % (link))
       return False
-   
    return True
 
 # Uses the keywords from the corpus to scrape Wikipedia
 # Returns a dictionary where key is URL and value is a heat map of other words in the dictionary
 def scrapeWikipedia():
+   # TESTING
+   test: int = 0
+   
    scrapedInfo: dict = {}
 
    for line in fileObject:
@@ -34,9 +35,19 @@ def scrapeWikipedia():
       
       keyword: str = line.strip("\n")
       newUrl: str = WIKI_URL_BASE + keyword
+      link: requests.models.Response = requests.get(newUrl)
 
-      if not isValidLink(newUrl):
+      if not isValidLink(link):
          continue
+      
+      soup = BeautifulSoup(link.content, "html.parser")
+      print(soup.get_text())
+
+      # TESTING
+      test += 1
+      if (test > 0):
+         break
+
 
 scrapeWikipedia()
 
