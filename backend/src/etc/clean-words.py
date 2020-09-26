da10bd4c12c809475a4b0d88c993c775a9571a9e
Adam Fernandes <fernadam8@gmail.com>
2020-09-25 22:19:10 -0400

    cleaned words in keywords.txt

---
+++ backend/src/etc/clean-words.py
@@ -0,0 +1,23 @@
+# Quick script for cleaning previous data
+
+# Read file
+fileObject = open("keywords.txt", "r")
+
+rawInput = fileObject.readlines()
+noiceInput = []
+
+for item in rawInput:
+   s = item
+   newS = item.split(", ")
+   noiceInput.append(newS)
+
+fileObject.close()
+
+# Writing back into file
+fileObject = open("keywords.txt", "w")
+
+for item in noiceInput:
+   for string in item:
+      fileObject.write(string + "\n")
+
+fileObject.close()
