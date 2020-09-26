fee777d0b531ecd743e55b8232275ee2ef2d782c
Adam Fernandes <fernadam8@gmail.com>
2020-09-25 23:17:57 -0400

    cleaned up previous code and threw into a function

--- backend/src/etc/web_scraper.py
+++ backend/src/etc/web_scraper.py
@@ -1,17 +1,23 @@
 # Adam Fernandes
 # ShellHacks 2020
 
+# URL bases which will be the beginning of all URLS
 WIKI_URL_BASE: str = "https://en.wikipedia.org/wiki/"
 
 # Opens up the political keywords corpus
 fileObject = open("keywords.txt", "r")
 
-for line in fileObject:
-   if line == "\n":
-      continue
-   keyword: str = line.strip("\n")
-   newUrl: str = WIKI_URL_BASE + keyword
+def scrapeWikipedia():
+   for line in fileObject:
+      if line == "\n":
+         continue
+      
+      keyword: str = line.strip("\n")
+      newUrl: str = WIKI_URL_BASE + keyword
 
-   print(newUrl)
+      print(newUrl)
 
+scrapeWikipedia()
+
+# Closes the file before terminating
 fileObject.close()
