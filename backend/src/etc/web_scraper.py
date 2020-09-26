0b849571634b214db7efda4a45440ef0e38cca0a
Adam Fernandes <fernadam8@gmail.com>
2020-09-25 23:12:25 -0400

    added scraping code for creating wikipedia URLs from all keywords

--- backend/src/etc/web_scraper.py
+++ backend/src/etc/web_scraper.py
@@ -1,1 +1,17 @@
-# TODO: Web scraping code should live here in the etc/ folder
+# Adam Fernandes
+# ShellHacks 2020
+
+WIKI_URL_BASE: str = "https://en.wikipedia.org/wiki/"
+
+# Opens up the political keywords corpus
+fileObject = open("keywords.txt", "r")
+
+for line in fileObject:
+   if line == "\n":
+      continue
+   keyword: str = line.strip("\n")
+   newUrl: str = WIKI_URL_BASE + keyword
+
+   print(newUrl)
+
+fileObject.close()
