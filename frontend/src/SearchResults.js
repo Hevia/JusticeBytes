b8d53923ca713e7c6f57f28fa66c73de9d129183
Hevia <anthony@hevia.dev>
2020-09-26 18:45:56 -0400

    Cleaned up search results page

--- frontend/src/SearchResults.js
+++ frontend/src/SearchResults.js
@@ -10,24 +10,28 @@ import { Button } from "baseui/button";
 function SearchResults(props) {
   return (
   <div>
+  <a href="/"><svg className="search-results-logo"></svg></a>
+  <div className="search-result-box">
     <ul>
         {
             props.stateContext.searchResults.get.map(searchResult => (
                 <Card className="search-result-card">
                     <StyledAction>
-                    <Button
-                    overrides={{
-                        BaseButton: { style: { width: "50%" } }
-                    }}
-                    href={searchResult.url}
-                    >
+                    <a href={searchResult.url}>
+                        <Button
+                        className="search-result-button"
+                        overrides={{
+                            BaseButton: { style: { width: "100%" } }
+                        }}>
                     {searchResult.title}
                     </Button>
+                    </a>
                 </StyledAction>
                 </Card>
             ))
         }
     </ul>
+    </div>
 </div>
   );
 }
